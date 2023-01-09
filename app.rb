bc5c84dcd4b41e1a1ec3bd198ddc4802ed022eb1
Joe Burgess <josephburgess@gmail.com>
2023-01-09 11:41:47 +0000

    initial tests for routing

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+  get '/hello' do
+    name = params[:name]
+    "Hello #{name}"
+  end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+    "Thanks #{name}, you sent this message: '#{message}'."
+  end
+
+  get '/names' do
+    'Julia, Mary, Karim'
+  end
+end
